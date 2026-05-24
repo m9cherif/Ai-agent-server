bdd9c889f6e751633a96b2eb97d3895eaa7d8094
mohamed <ense1077710@tarbia.tn>
2026-05-24 23:05:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,71 @@
+const express = require("express");
+const cors = require("cors");
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash"
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "AI Agent Running"
+  });
+});
+
+app.post("/chat", async (req, res) => {
+
+  try {
+
+    const msg = req.body.message;
+
+    if(!msg){
+      return res.json({
+        error:"No message"
+      });
+    }
+
+    const result = await model.generateContent({
+      contents: [
+        {
+          role: "user",
+          parts: [
+            {
+              text: msg
+            }
+          ]
+        }
+      ]
+    });
+
+    const text =
+      result.response.candidates[0]
+      .content.parts[0]
+      .text;
+
+    res.json({
+      response: text
+    });
+
+  } catch(err){
+
+    console.log(err);
+
+    res.json({
+      error: err.message
+    });
+
+  }
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Running");
+});
