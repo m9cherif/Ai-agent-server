549de72daae91c0178785c9e5db28070a37fdf88
mohamed <ense1077710@tarbia.tn>
2026-05-24 23:00:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
@@ -7,15 +6,13 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-pro",
 });
-
-let memory = [];
 
 app.get("/", (req, res) => {
   res.json({
@@ -29,35 +26,19 @@ app.post("/chat", async (req, res) => {
 
     const msg = req.body.message;
 
-    memory.push({
-      user: msg
-    });
+    const result = await model.generateContent(msg);
 
-    const prompt = `
-    You are a smart AI Agent.
-
-    Memory:
-    ${JSON.stringify(memory)}
-
-    User:
-    ${msg}
-    `;
-
-    const result = await model.generateContent(prompt);
-
-    const response = await result.response;
+    const response = result.response;
 
     const text = response.text();
-
-    memory.push({
-      ai: text
-    });
 
     res.json({
       response: text
     });
 
-  } catch(err){
+  } catch (err) {
+
+    console.log(err);
 
     res.json({
       error: err.message
