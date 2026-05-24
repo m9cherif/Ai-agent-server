78105cd93c2d71ffef2e44f6eade198f8058411c
mohamed <ense1077710@tarbia.tn>
2026-05-24 23:31:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,23 +18,31 @@ app.post("/chat", async (req, res) => {
 
     const msg = req.body.message;
 
+    if(!msg){
+      return res.json({
+        error: "No message"
+      });
+    }
+
     const response = await fetch(
       "https://openrouter.ai/api/v1/chat/completions",
       {
         method: "POST",
 
         headers: {
+
           "Authorization":
             "Bearer " + process.env.API_KEY,
 
           "Content-Type":
             "application/json"
+
         },
 
         body: JSON.stringify({
 
           model:
-            "mistralai/mistral-7b-instruct",
+            "openai/gpt-3.5-turbo",
 
           messages: [
             {
@@ -50,8 +58,27 @@ app.post("/chat", async (req, res) => {
 
     const data = await response.json();
 
+    console.log(data);
+
+    if(data.error){
+      return res.json({
+        error: data.error.message
+      });
+    }
+
+    if(
+      !data.choices ||
+      !data.choices[0]
+    ){
+      return res.json({
+        error: "No AI response"
+      });
+    }
+
     const text =
-      data.choices[0].message.content;
+      data.choices[0]
+      .message
+      .content;
 
     res.json({
       response: text
